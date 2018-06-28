171f9c4726bf1a52323722ca540b6a5bf120b471
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-06-28 02:09:05 +0200

    Feat: Add maps to site by using map box
    Add map token in main.js & restaurant_info.js
    Add stylesheets link in head of index.html & restaurant.html
    Add script tags before body closing tag in index.html & restaurant.html

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoic2FyYWgyN2giLCJhIjoiY2ppeHE2c2t0MDJlNDN3b2xla2hpMWNhbSJ9.1u_PFCvPclRUQnldkzwEuA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
