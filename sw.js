63565aea938682182d4664d2042752f5961a555e
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-07-05 00:51:23 +0200

    Feat: add cache response

--- sw.js
+++ sw.js
@@ -20,15 +20,29 @@ self.addEventListener('install', function(event) {
                 'img/7.jpg',
                 'img/8.jpg',
                 'img/9.jpg',
-                'img/10.jpg',
-                'https://api.mapbox.com/mapbox-gl-js/v0.46.0/mapbox-gl.css',
-                'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-                'https://api.mapbox.com/mapbox-gl-js/v0.46.0/mapbox-gl.js',
-                'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
-                'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png',
-                'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
-                'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1204/1540.jpg70?access_token=pk.eyJ1Ijoic2FyYWgyN2giLCJhIjoiY2ppeHE2c2t0MDJlNDN3b2xla2hpMWNhbSJ9.1u_PFCvPclRUQnldkzwEuA'
+                'img/10.jpg'
+                // 'https://api.mapbox.com/mapbox-gl-js/v0.46.0/mapbox-gl.css',
+                // 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+                // 'https://api.mapbox.com/mapbox-gl-js/v0.46.0/mapbox-gl.js',
+                // 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+                // 'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png',
+                // 'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
+                // 'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1204/1540.jpg70?access_token=pk.eyJ1Ijoic2FyYWgyN2giLCJhIjoiY2ppeHE2c2t0MDJlNDN3b2xla2hpMWNhbSJ9.1u_PFCvPclRUQnldkzwEuA'
             ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event){
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if(response) return response;
+            return fetch(event.request);
+        })
+        .catch(function(error) {
+            console.log('error');
         })
     );
 });
