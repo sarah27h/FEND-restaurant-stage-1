64e689d2cac0c0c6251078ebe7e869aa470589a8
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-07-07 00:56:13 +0200

    Test: using '/' in cache array

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@ let staticCacheName = 'restaurant-static-v1';
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('staticCacheName').then(function(cache) {
+        caches.open('staticCacheName').then(function(cache){
             return cache.addAll([
-                './',
+                '/',
                 './index.html',
                 './restaurant.html',
                 // './restaurant.html?id=1',
@@ -33,17 +33,57 @@ self.addEventListener('install', function(event) {
                 './img/8.jpg',
                 './img/9.jpg',
                 './img/10.jpg'
-            ]);
+            ])
         })
     );
 });
 
+// self.addEventListener('install', (event) => {
+//     event.waitUntil(
+//         caches.open('staticCacheName').then(cache => {
+//             return cache.addAll([
+//                 './',
+//                 './index.html',
+//                 './restaurant.html',
+//                 // './restaurant.html?id=1',
+//                 // './restaurant.html?id=2',
+//                 // './restaurant.html?id=3',
+//                 // './restaurant.html?id=4',
+//                 // './restaurant.html?id=5',
+//                 // './restaurant.html?id=6',
+//                 // './restaurant.html?id=7',
+//                 // './restaurant.html?id=8',
+//                 // './restaurant.html?id=9',
+//                 // './restaurant.html?id=10',
+//                 './css/styles.css',
+//                 './js/dbhelper.js',
+//                 './js/main.js',
+//                 './js/regist.js',
+//                 './js/restaurant_info.js',
+//                 './data/restaurants.json',
+//                 './img/1.jpg',
+//                 './img/2.jpg',
+//                 './img/3.jpg',
+//                 './img/4.jpg',
+//                 './img/5.jpg',
+//                 './img/6.jpg',
+//                 './img/7.jpg',
+//                 './img/8.jpg',
+//                 './img/9.jpg',
+//                 './img/10.jpg'
+//             ])
+//         })
+//     );
+// });
+
 self.addEventListener('fetch', function(event){
     console.log(event.request.url);
+    
     event.respondWith(
         caches.match(event.request)
         .then(function(response) {
-            
+            console.log(this);
+
             if(response) return response;
             console.log(response);
             console.log(event.request);
@@ -54,6 +94,26 @@ self.addEventListener('fetch', function(event){
         })
     );
 });
+
+// self.addEventListener('fetch', (event) => {
+//     console.log(self);
+//     console.log(event.request.url);
+//     event.respondWith(
+//         caches.match(event.request)
+//         .then((response) => {
+//             console.log(this);
+
+//             if(response) return response;
+//             console.log(response);
+//             console.log(event.request);
+//             return fetch(event.request);
+//         })
+//         .catch((error) => 
+//             console.log('error cannot fetch')
+//         )
+//     );
+// });
+
 
 self.addEventListener('activate', function(event) {
     event.waitUntil(
@@ -69,3 +129,18 @@ self.addEventListener('activate', function(event) {
         }) 
     );
 });
+
+// self.addEventListener('activate', (event) => {
+//     event.waitUntil(
+//         caches.keys().then(cacheNames => {
+//             return Promise.all(
+                
+//                 cacheNames.filter(cacheName => {
+//                     return cacheName.startsWith('restaurant-static') && cacheName != staticCacheName;
+//                 }).map(cacheName => {
+//                     return caches.delete(cacheName);
+//                 })
+//             )
+//         }) 
+//     );
+// });
