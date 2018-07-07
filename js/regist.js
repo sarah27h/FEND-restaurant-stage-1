dc1216358c79e4e3482760b4e5e2110cfe909c77
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-07-07 02:07:56 +0200

    Style: formatting code

--- js/regist.js
+++ js/regist.js
@@ -1,8 +1,8 @@
 // check if brower support SW
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('./sw.js').then(function() {
-        console.log('registration worked');
-    }).catch(function() {
-        console.log('registration failed :)');
-    });
+    //register SW
+    navigator.serviceWorker.register('./sw.js').then( () => 
+        console.log('registration succeed :)'))
+        .catch( () =>
+        console.log('registration failed :('));
 }
